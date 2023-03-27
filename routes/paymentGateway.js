9cbf713d9dd38180d32a257a6ffe7e9a98ab078c
Thanishrishi <thanishrishii@gmail.com>
2023-03-27 18:55:07 +0530

    thaish

--- routes/paymentGateway.js
+++ routes/paymentGateway.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const router = express.Router();
 const mongoose = require('mongoose');
@@ -8,21 +7,8 @@ require('dotenv').config();
 
 router.post('/paymentgateway',(req,res)=>{
 
+    console.log("Thanish")
     res.send(req.body);
 })
 
-=======
-const express = require("express");
-const router = express.Router();
-const mongoose = require('mongoose');
-const registeredUsers = require('../models/registeredUsers');
-require('dotenv').config();
-
-
-router.post('/paymentgateway',(req,res)=>{
-
-    res.send(req.body);
-})
-
->>>>>>> 5f806983651c972443a0229d7ec30326501e19ba
 module.exports = router;
