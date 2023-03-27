5f806983651c972443a0229d7ec30326501e19ba
Thanishrishi <117743286+Thanishrishi@users.noreply.github.com>
2023-03-27 18:23:45 +0530

    pay,enggd5

--- routes/paymentGateway.js
+++ routes/paymentGateway.js
@@ -6,8 +6,8 @@ require('dotenv').config();
 
 
 router.post('/paymentgateway',(req,res)=>{
-    console.log(req);
-    res.send("success");
+
+    res.send(req.body);
 })
 
 module.exports = router;
