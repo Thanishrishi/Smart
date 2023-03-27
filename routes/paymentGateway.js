269a2561c3c0475b6ac0a90926c00d7657632fe9
Thanishrishi <thanishrishii@gmail.com>
2023-03-27 21:38:46 +0530

    hugepaymentchanges

--- routes/paymentGateway.js
+++ routes/paymentGateway.js
@@ -2,22 +2,51 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require('mongoose');
 const registeredUsers = require('../models/registeredUsers');
+const Bill = require('../models/Bill');
 require('dotenv').config();
 
 
 router.post('/paymentgateway',async(req,res)=>{
     const{username , amount , billdata } = req.body;
     registeredUsers.findOne({Username : username})
-    .then((saveduser)=>{
-        let wallet = saveduser.Wallet;
-        console.log(wallet);
-        if(wallet < amount){
-            res.send("Insufficientfunds");
-            return;
+    .then(async(saveduser)=>{
+        if(saveduser){
+            console.log(saveduser);
+            let wallet = saveduser.Wallet;
+            wallet = parseInt(wallet);
+            console.log(wallet + amount);
+            if(wallet < amount){
+                
+                res.send("Insufficientfunds");
+                return;
+            }else{
+                let reducedamount = wallet - amount;
+                registeredUsers.updateOne({Username : username},{Wallet : reducedamount},(err,docs)=>{
+                    if(err){
+                        console.log(err);
+                    }else{
+                        console.log("Updated " + docs);
+                    }
+                })
+                const newbill =  Bill();
+                try{
+                    newbill.Name = username;
+                    newbill.Totalamount = amount;
+                    newbill.Items = billdata;
+                   const ret =  await newbill.save();
+       
+                    res.send(ret._id);
+                    return;
+                }catch(error){
+                    console.log(error);
+                }
+            }
+        }else{
+            console.log("usernotfound")
         }
     })
 
-    res.send("successs")
+  
 })
 
 module.exports = router;
